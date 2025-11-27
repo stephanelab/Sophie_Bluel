825441dc59b7e05ff96491451f8aed2ad0295c66
Stéphane LABROT <stephane.labrot@gmail.com>
2025-11-27 10:16:05 +0100

    Ajout des boutons de filtre des travaux

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,9 +1,8 @@
+// const { createElement } = require("react")
+
 // Récupération des travaux depuis l'API
 const reponse = await fetch ("http://localhost:5678/api/works")
 const works = await reponse.json()
-
-// Transformation des travaux en JSON
-// const worksJSON = JSON.stringify(works)
 
 // Génération des travaux dans la galerie
 function generateWorks(works) {
@@ -31,4 +30,32 @@ function generateWorks(works) {
         workElement.appendChild(captionElement)
     }
 }
+
+// Ajout des boutons de filtre des travaux
+const divFilterButtons = document.createElement("div")
+divFilterButtons.classList.add("filter-buttons")
+document.querySelector("#portfolio").insertBefore(divFilterButtons, document.querySelector(".gallery"))
+
+
+const buttonAll = document.createElement("button")
+buttonAll.classList.add("all", "active")
+buttonAll.innerText = "Tous"
+
+const buttonObjects = document.createElement("button")
+buttonObjects.classList.add("objects")
+buttonObjects.innerText = "Objets"
+
+const buttonAppartments = document.createElement("button")
+buttonAppartments.classList.add("appartments")
+buttonAppartments.innerText = "Appartements"
+
+const buttonHotelsRestaurants = document.createElement("button")
+buttonHotelsRestaurants.classList.add("hotels_restaurants")
+buttonHotelsRestaurants.innerText = "Hotels & restaurants"
+
+divFilterButtons.appendChild(buttonAll)
+divFilterButtons.appendChild(buttonObjects)
+divFilterButtons.appendChild(buttonAppartments)
+divFilterButtons.appendChild(buttonHotelsRestaurants)
+
 generateWorks(works)
