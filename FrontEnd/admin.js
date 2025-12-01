e9759953071f694e21f9118218d2dcbee8af9cb4
Stéphane LABROT <stephane.labrot@gmail.com>
2025-12-01 12:05:17 +0100

    ajout message d'erreur sur mauvaise identification

--- FrontEnd/admin.js
+++ FrontEnd/admin.js
@@ -10,17 +10,28 @@ connexion.addEventListener("submit", async function(event) {
     const loginDataJson = JSON.stringify(loginData)
 
     // Envoi de la requête POST au serveur
+    let token = ""
     try {
         const response = await fetch("http://localhost:5678/api/users/login", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: loginDataJson
         })
+        if (!response.ok) {
+            throw new Error("Utilisateur ou mot de passe incorrect")
+        }
         const Data = await response.json()
-        const token = Data.token
+        token = Data.token
         localStorage.setItem("token", token)
         window.location.href = "index.html"
     } catch (error) {
         console.error("Erreur lors de la requête :", error)
+        // Ajout des boutons de filtre des travaux
+        const texteErreur = document.createElement("p")
+        texteErreur.style.color = "red"
+        texteErreur.style.fontSize = "18px"
+        texteErreur.style.textDecoration = "none"
+        texteErreur.innerText = "Utilisateur ou mot de passe incorrect"
+        document.querySelector("#login-section").insertBefore(texteErreur, document.querySelector("p"))
     }
 })
